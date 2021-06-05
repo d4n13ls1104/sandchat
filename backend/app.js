5519adb0bd75848fb69583bfa31b582c3441b1e0
d4n13ls1104 <78829103+d4n13ls1104@users.noreply.github.com>
2021-06-05 03:50:33 -0400

    Add middleware needed for other updated files

--- backend/app.js
+++ backend/app.js
@@ -1,16 +1,20 @@
 const express = require('express');
 const path = require('path');
+const cookieParser = require("cookie-parser");
+
 const indexRouter = require("./routes/index");
 const userRouter = require("./routes/user");
+
 const app = express();
 
 const port = process.env.PORT || 80;
 
+app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true}));
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/', indexRouter);
 app.use("/user", userRouter);
+app.use('/', indexRouter);
 
 app.listen(port, () => console.log(`Server listening on port ${port}`));
