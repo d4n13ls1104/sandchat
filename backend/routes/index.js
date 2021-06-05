8eb3804f11a912a25dc7dee1b116f98345f9af8b
d4n13ls1104 <78829103+d4n13ls1104@users.noreply.github.com>
2021-06-05 03:54:02 -0400

    Use auth middleware

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,9 +1,12 @@
-var express = require('express');
+const express = require('express');
 const path = require("path");
-var router = express.Router();
+const auth = require("../middleware/sand-auth");
+
+const router = express.Router();
 
 /* GET home page. */
-router.get('*', (req, res , next) => {
+router.get('*', auth, (req, res) => {
+  if((req.baseUrl + req.path) === "/login" && req.cookies.auth) return res.redirect("/home");
   res.sendFile(path.resolve(__dirname, "../public/index.html"));
 });
 
