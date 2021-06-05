2ded4755c70a651609aa5b87ebb4e7ef9a22d170
d4n13ls1104 <78829103+d4n13ls1104@users.noreply.github.com>
2021-06-05 04:28:49 -0400

    Rewrite as functional component

--- frontend/src/components/Register.js
+++ frontend/src/components/Register.js
@@ -1,6 +1,7 @@
-import React from "react";
-import "../stylesheets/register_styles.css";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
+
+import "../stylesheets/register_styles.css";
 
 import Error from "./Error";
 import Success from "./Success";
@@ -13,71 +14,46 @@ const initialState = {
     errors: []
 }
 
-class Register extends React.Component {
-    constructor() {
-        super();
-        this.state = initialState;
+const Register = () => {
+    const [state, setState] = useState(initialState);
 
-        this.updateEmail = this.updateEmail.bind(this);
-        this.updateUsername = this.updateUsername.bind(this);
-        this.updatePassword = this.updatePassword.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
-        this.handleKeyDown = this.handleKeyDown.bind(this);
+    useEffect(() => {
+        window.addEventListener("keydown", handleKeyDown);
+
+        return () => {
+            window.removeEventListener("keydown", handleKeyDown);
+        }
+    });
+
+    const handleKeyDown = (e) => {
+        if(e.keyCode === 13) handleSubmit();
     }
 
-    updateEmail(e) {
-        this.setState({ email: e.target.value });
-    }
-
-    updateUsername(e) {
-        this.setState({ username: e.target.value });
-    }
-
-    updatePassword(e) {
-        this.setState({ password: e.target.value });
-    }
-
-    handleSubmit() {
-        axios.post("/user/register", `email=${this.state.email}&username=${this.state.username}&password=${this.state.password}`)
+    const handleSubmit = () => {
+        setState({...state, loading: true});
+        axios.post("/user/register", `email=${state.email}&username=${state.username}&password=${state.password}`)
         .then(res => {
-            if(res.data.ok) {
-                this.setState({ errors: [] });
-                this.setState({ success: true });
-            }
-            if(res.data.errors) {
-                this.setState({ errors: res.data.errors });
-                this.setState({ success: false });
-            }
+            if(res.data.ok) setState({...state, success: true, errors: []});
+            if(res.data.errors) setState({...state, errors: res.data.errors, success: false});
         });
-        this.setState(initialState);
     }
 
-    handleKeyDown(e) {
-        if(e.keyCode == 13) this.handleSubmit();
-    }
-
-    render() {
-        var errors;
-        var success;
-        if(this.state.success) success = <Success message="Account successfully created!"/>
-        if(this.state.errors.length > 0) errors = <Error message={this.state.errors[0]}/>
-        return (
-            <>
-                <div id="form-wrapper" tabvIndex="0" onKeyDown={this.handleKeyDown}>
-                    <h1>Register</h1>
-                    <input onChange={this.updateEmail} value={this.state.email} type="text" placeholder="Email address" className="input"/>
-                    <input onChange={this.updateUsername} value={this.state.username} type="text" placeholder="Username" className="input"/>
-                    <input onChange={this.updatePassword} value={this.state.password} type="password" placeholder="Password" className="input"/>
-                    <button onClick={this.handleSubmit} type="submit">Register</button>
-                    <span id="form-text">Already have an account? <a href="/login">Login</a>!</span>
-                </div>
-                <div id="error-wrapper">
-                    {errors}
-                    {success}
-                </div>
-            </>
-        );
-    }
+    return (
+        <>
+            <div id="form-wrapper">
+                <h1>Register</h1>
+                <input onChange={(e) => setState({...state, email: e.target.value})} value={state.email} type="email" placeholder="Email address" className="input"/>
+                <input onChange={(e) => setState({...state, username: e.target.value})} value={state.username} type="text" placeholder="Username" className="input"/>
+                <input onChange={(e) => setState({...state, password: e.target.value})} value={state.password} type="password" placeholder="Password" className="input"/>
+                <button onClick={handleSubmit}>Register</button>
+                <span id="form-text">Already have an account? <a href="/login">Login</a>!</span>
+            </div>
+            <div id="alert-wrapper">
+                {state.errors.length > 0 ? <Error message={state.errors[0]}/> : null}
+                {state.success ? <Success message="Account created successfully!"/> : null}
+            </div>
+        </>
+    );
 }
 
 export default Register;
