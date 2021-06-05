0991bbc0bcc4741629d13eebed99ccfc3077acce
d4n13ls1104 <78829103+d4n13ls1104@users.noreply.github.com>
2021-06-05 04:25:40 -0400

    Rewrite as functional component

--- frontend/src/components/IndexPage.js
+++ frontend/src/components/IndexPage.js
@@ -1,30 +1,25 @@
 import React from "react";
 import "../stylesheets/index_styles.css";
 
-class IndexPage extends React.Component {
-    render() {
-        return (
-            <>
-            <header>
-                <img src="/images/logo.png" id="logo" alt=""/>
-                <h1>Sandchat</h1>
-            </header>
-            <div id="form-wrapper">
-            <div id="form">
-                <div id="form-head">
-                    <h2>It's time to ditch Discord.</h2>
-                    <p>By continuing you agree to our <a href="#">Privacy Policy</a> and <a href="#">Terms of Service</a></p>
+const IndexPage = () => {
+    return (
+        <>
+        <header>
+            <img src="/images/logo.png" id="logo" alt=""/>
+            <h1>Sandchat</h1>
+        </header>
+        <div id="form-wrapper">
+        <div id="form">
+            <div id="form-head">
+                <h2>It's time to ditch Discord.</h2>
+                <p>By continuing you agree to our <a href="#">Privacy Policy</a> and <a href="#">Terms of Service</a></p>
                 </div>
-                <div id="nav-wrapper">
-                    <a href="/login" className="button-link">
-                        <div class="button" style={{marginTop:30}}>
-                            Login
-                        </div>
-                    </a>
-                    <a href="/register" className="button-link">
-                        <div class="button">
-                            Register
-                        </div>
+            <div id="nav-wrapper">
+                <a href="/login" className="button-link">
+                    <div class="button" style={{marginTop:30}}>Login</div>
+                </a>
+                <a href="/register" className="button-link">
+                    <div class="button">Register</div>
                     </a>
                 </div>
             </div>
@@ -35,9 +30,7 @@ class IndexPage extends React.Component {
             <a href="https://www.guilded.gg/i/WknKxlBp">Join us on Guilded</a>
         </footer>
         </>
-        );
-    }
-
+    );
 }
 
 export default IndexPage;
