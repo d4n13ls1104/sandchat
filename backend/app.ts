0f684562a59f946a881e1b9fff7c00d26e9c4236
d4n13ls1104 <78829103+d4n13ls1104@users.noreply.github.com>
2021-06-07 05:56:57 -0400

    Rewrite to typescript

---
+++ backend/app.ts
@@ -0,0 +1,23 @@
+import express, { json, urlencoded, static as _static} from "express";
+import { join } from "path";
+import cookieParser from "cookie-parser";
+
+import indexRouter from "./routes/index";
+import userRouter from "./routes/user";
+
+import whitelist from "./middleware/whitelist";
+
+const app = express();
+
+const port = process.env.PORT || 80;
+
+app.use(whitelist);
+app.use(cookieParser());
+app.use(json());
+app.use(urlencoded({ extended: true }));
+app.use(_static(join(__dirname, "public")));
+
+app.use("/user", userRouter);
+app.use("/", indexRouter);
+
+app.listen(port, () => console.log(`Server listening on port ${port}`));
