d43c617e07fd9918132a2ed9a6beddba98c8091a
d4n13ls1104 <78829103+d4n13ls1104@users.noreply.github.com>
2021-06-07 05:33:41 -0400

    Rewrite to typescript

--- backend/util/sand-db.js
+++
@@ -1,8 +0,0 @@
-const config = require("../misc/config.json"); // DB Connection info
-const mysql = require("mysql"); // MySQL lib
-
-const pool = mysql.createPool(config);
-
-module.exports = {
-    pool
-};
