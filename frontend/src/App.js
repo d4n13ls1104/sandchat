7f81482d920656de0e919722ac99b9edf716b510
d4n13ls1104 <78829103+d4n13ls1104@users.noreply.github.com>
2021-06-08 22:01:41 -0400

    Rewrite to typescript

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,18 @@
-import IndexPage from "./components/IndexPage";
-import Register from "./components/Register";
-
+import React from 'react';
 import { BrowserRouter as Router, Route } from "react-router-dom";
 
-const App = () => {
+import IndexPage from "./components/IndexPage";
+import Register from "./components/Register";
+import Login from "./components/Login";
+import "./stylesheets/register_styles.css";
+import "./stylesheets/index_styles.css";
+
+const App: React.FC = () => {
   return (
     <Router>
       <Route exact path="/" component={IndexPage}/>
       <Route exact path="/register" component={Register}/>
+      <Route exact path="/login" component={Login}/>
     </Router>
   );
 }
