325d5a67487b07d44c1d426bf8099678de5c6734
d4n13ls1104 <78829103+d4n13ls1104@users.noreply.github.com>
2021-06-09 13:58:57 -0400

    Update sand-user.ts

--- backend/util/sand-user.ts
+++ backend/util/sand-user.ts
@@ -1,8 +1,7 @@
 import { escape } from "mysql";
 import { pool } from "./sand-db";
-import { sign, verify } from "./sand-jwt";
 
-const _DEFAULT_ACCESS_TOKEN_LIFE = 604800; // Access token life in seconds
+export const _DEFAULT_ACCESS_TOKEN_LIFE = 604800; // Access token life in seconds
 
 interface UserAccount {
     email: string,
@@ -11,13 +10,15 @@ interface UserAccount {
     tokenVersion?: number
 };
 
+type AccountCallback = (account: UserAccount) => void
+
 export const createUserAccount = (account: UserAccount): boolean => {
     try {
         pool.getConnection((err, connection) => {
             if(err) throw new Error(err.message);
             
             connection.query(`INSERT INTO users (email, username, password) VALUES (${escape(account.email)}, ${escape(account.username)}, ${escape(account.password)})`, (err) => {
-                throw new Error(err.message);
+                if(err) throw new Error(err.message);
             });
             connection.release();
         });  
@@ -30,7 +31,7 @@ export const createUserAccount = (account: UserAccount): boolean => {
 
 export const deleteUserAccount = (id: number): boolean => {
     try {
-        pool.getConnection((err, connection) => {
+        pool.getConnection((err, connection) =>     {
             if(err) throw new Error(err.message);
 
             connection.query(`DELETE FROM users WHERE id=${escape(id)}`, (err) => {
@@ -45,53 +46,29 @@ export const deleteUserAccount = (id: number): boolean => {
     return true;
 }
 
-export const getUserAccountById = (id: number): UserAccount | boolean => {
-    try {
-        pool.getConnection((err, connection) => {
-            if(err) throw new Error(err.message);
+export const getUserAccountById = (id: number, callback: AccountCallback): UserAccount | boolean => {
+    pool.getConnection((err, connection) => {
             
-            connection.query(`SELECT email, username, password FROM users WHERE id=${escape(id)}`, (err, results) => {
-                if(err) throw new Error(err.message);
-                if(results.length === 0) throw new Error("No user with that id!");
-                
-                return {
-                    email: results[0].email,
-                    username: results[0].username,
-                    password: results[0].password,
-                    tokenVersion: results[0].tokenVersion
-                };
+        connection.query(`SELECT email, username, password, tokenVersion FROM users WHERE id='${escape(id)}'`, (err, results) => {
+            if(results.length === 0) console.log("ERROR");
+
+            callback({
+                email: results[0].email,
+                username: results[0].username,
+                password: results[0].password,
+                tokenVersion: results[0].tokenVersion
             });
-            connection.release();
+            
+            return {
+                email: results[0].email,
+                username: results[0].username,
+                password: results[0].password,
+                tokenVersion: results[0].tokenVersion
+            };
         });
-    } catch {
-        return false;
-    }
-}
-
-export const signTokenForUser = (id: number, refresh?: boolean): string | boolean => {
-    const user = getUserAccountById(id);
-    if(typeof user != "object") return false;
-
-    return sign({
-        alg: "HS256",
-        type: refresh ? "refresh" : "access"
-    },
-    {
-        sub: id,
-        username: user.username,
-        version: user.tokenVersion,
-        iat: new Date().getTime() / 1000,
-        exp: (new Date().getTime() / 1000) + _DEFAULT_ACCESS_TOKEN_LIFE
-        
-    },
-    refresh);
-}
-
-export const verifyTokenForUser = (id: number, token: string, refresh: boolean): boolean => {
-    const user = getUserAccountById(id);
-    if(typeof user != "object") return false;
-
-    return verify(token, user.tokenVersion, refresh);
+        connection.release();
+    });
+    return false;
 }
 
 export const revokeTokensForUser = (id: number): boolean => {
@@ -108,4 +85,5 @@ export const revokeTokensForUser = (id: number): boolean => {
         console.error(`Could not revoke tokens for user: ${id}`);
         return false;
     }
+    return false;
 }
