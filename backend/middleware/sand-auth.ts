d12726ee7f4014e100d17e1913d7899d01fea3b5
d4n13ls1104 <78829103+d4n13ls1104@users.noreply.github.com>
2021-06-09 13:55:58 -0400

    Update sand-auth.ts

--- backend/middleware/sand-auth.ts
+++ backend/middleware/sand-auth.ts
@@ -1,17 +1,32 @@
-import { verify } from "../util/sand-jwt";
+import { pool } from "../util/sand-db";
+import { extractPayloadFromToken, verify } from "../util/sand-jwt";
 
 const whitelisted_paths = [
     "/",
     "/login",
-    "/register"
+    "/register",
+    "/user/count",
 ];
 
 const auth = (req: any, res: any, next: any) => {
     if(!whitelisted_paths.includes(req.baseUrl + req.path)) {
-        if(!req.cookies.auth || !verify(req.cookies.auth)) {
+        if(!req.cookies.auth || req.cookies.auth.split(".").length != 3) {
             res.clearCookie("auth");
             return res.redirect("/login");
         }
+
+        const payload = extractPayloadFromToken(req.cookies.auth); // bug occurs here
+        
+        pool.getConnection((err, connection) => {
+            connection.query(`SELECT tokenVersion FROM users WHERE id=${escape(""+payload.sub)}`, (err, result) => {
+                if(result.length === 0) return res.redirect("/login");
+                if(!verify(req.cookies.auth, result[0].tokenVersion)) {
+                    res.clearCookie("auth");
+                    res.redirect("/login");
+                    return;
+                }
+            });
+        })
     }
     next();
 }
