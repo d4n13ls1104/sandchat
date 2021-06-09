e9f98c5791a95614bce73d9dca30a9e44ad42489
d4n13ls1104 <78829103+d4n13ls1104@users.noreply.github.com>
2021-06-09 14:01:08 -0400

    Update App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,6 +4,8 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 import IndexPage from "./components/IndexPage";
 import Register from "./components/Register";
 import Login from "./components/Login";
+import Home from "./components/Home";
+
 import "./stylesheets/register_styles.css";
 import "./stylesheets/index_styles.css";
 
@@ -13,6 +15,7 @@ const App: React.FC = () => {
       <Route exact path="/" component={IndexPage}/>
       <Route exact path="/register" component={Register}/>
       <Route exact path="/login" component={Login}/>
+      <Route exact path="/home" component={Home}/>
     </Router>
   );
 }
