bb6d4c5992e538d8d420d0796f34ca0948590d19
d4n13ls1104 <78829103+d4n13ls1104@users.noreply.github.com>
2021-06-09 13:56:54 -0400

    Update index.ts

--- backend/routes/index.ts
+++ backend/routes/index.ts
@@ -1,10 +1,9 @@
 import { Router } from "express";
 import { resolve } from "path";
-import auth from "../middleware/sand-auth";
 
 const router = Router();
 
-router.get("*", auth, (req, res) => {
+router.get("*", (req, res) => {
     if((req.baseUrl + req.path) === "/login" && req.cookies.auth) return res.redirect("/home");
     res.sendFile(resolve(__dirname, "../public/index.html"));
 });
