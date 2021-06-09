0110ccd0d00945cccc52bf5258fdd91de76c9829
d4n13ls1104 <78829103+d4n13ls1104@users.noreply.github.com>
2021-06-09 13:58:29 -0400

    Update sand-jwt.ts

--- backend/util/sand-jwt.ts
+++ backend/util/sand-jwt.ts
@@ -15,22 +15,24 @@ export interface Payload {
     exp: number
 };
 
-export const sign = (header: Header, payload: Payload, refresh?: boolean): string => {
-    const secret = refresh ? jwt.refreshSecret : jwt.secret;
+export const extractPayloadFromToken = (token: string): Payload => {
+    return JSON.parse(base64.decode(token.split(".")[1])); // ez
+}
+
+export const sign = (header: Header, payload: Payload): string => {
 
     const token = `${base64(JSON.stringify(header))}.${base64(JSON.stringify(payload))}` // Create first and second segments
-    const signature = base64(sha256(token + secret));
+    const signature = base64(sha256(token + jwt.secret));
 
     return `${token}.${signature}`;
 }
 
-export const verify = (token: string, tokenVersion?: number, refresh?: boolean): boolean => {
-    const secret = refresh ? jwt.refreshSecret : jwt.secret;
+export const verify = (token: string, tokenVersion: number): boolean => {
     
     const segments = token.split("."); // separate the token segments
     if(segments.length != 3) return false; // If token doesn't contain 3 segments it is invalid therefore return false
 
-    if(base64(sha256(segments[0] + "." + segments[1] + secret)) != segments[2]) return false; // if token signature is invalid return false
+    if(base64(sha256(segments[0] + "." + segments[1] + jwt.secret)) != segments[2]) return false; // if token signature is invalid return false
     if(JSON.parse(base64.decode(segments[1])).exp < Math.floor(new Date().getTime() / 1000)) return false; // if token is expired return false
     if(JSON.parse(base64.decode(segments[1])).version != tokenVersion) return false; // if token version doesnt match token version on user return false
 
