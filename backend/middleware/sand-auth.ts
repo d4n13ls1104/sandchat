19f9482618ebb2d3c4266803ca678f75d7865bc2
d4n13ls1104 <78829103+d4n13ls1104@users.noreply.github.com>
2021-06-18 18:15:40 -0400

    UPDATE: Refactored auth middleware.

--- backend/middleware/sand-auth.ts
+++ backend/middleware/sand-auth.ts
@@ -1,32 +1,52 @@
+import { NextFunction, Request, Response } from "express";
 import { pool } from "../util/sand-db";
 import { extractPayloadFromToken, verify } from "../util/sand-jwt";
 
+// -----------------------------------------------------------
+// Purpose: Define paths which do not require authentication.
+// -----------------------------------------------------------
 const whitelisted_paths = [
     "/",
     "/login",
     "/register",
-    "/user/count",
+    "/api/user/count",
+    "/api/user/register",
+    "/api/user/login"
 ];
 
-const auth = (req: any, res: any, next: any) => {
+
+// -----------------------------------------------------------
+// Purpose: Auth middleware
+// -----------------------------------------------------------
+const auth = (req: Request, res: Response, next: NextFunction) => {
     if(!whitelisted_paths.includes(req.baseUrl + req.path)) {
+        // If path requires authentication:
         if(!req.cookies.auth || req.cookies.auth.split(".").length != 3) {
-            res.clearCookie("auth");
-            return res.redirect("/login");
+            res.clearCookie("auth"); // Get rid of the auth cookie so a redirect loop doesn't happen
+            res.redirect("/login"); // Redirect user to login page
+            return;
         }
 
-        const payload = extractPayloadFromToken(req.cookies.auth); // bug occurs here
+        const payload = extractPayloadFromToken(req.cookies.auth); // Get user info from token
         
         pool.getConnection((err, connection) => {
-            connection.query(`SELECT tokenVersion FROM users WHERE id=${escape(""+payload.sub)}`, (err, result) => {
-                if(result.length === 0) return res.redirect("/login");
+            connection.query(`SELECT tokenVersion FROM users WHERE id=${escape(payload.sub.toString())}`, (err, result) => {
                 if(!verify(req.cookies.auth, result[0].tokenVersion)) {
+                    // If token is invalid murder the token and redirect user to login
                     res.clearCookie("auth");
-                    res.redirect("/login");
-                    return;
+                    console.log(req.baseUrl + req.path);
+                    if((req.baseUrl + req.path).startsWith("/api")) {
+                        return res.sendStatus(403).json({ ok: false });
+                    }
+                    return res.redirect("/login");
                 }
+
+                if(result.length === 0) {
+                    return res.redirect("/login");
+                } // If no valid user is found in token payload redirect user to login
             });
-        })
+            connection.release();
+        });
     }
     next();
 }
