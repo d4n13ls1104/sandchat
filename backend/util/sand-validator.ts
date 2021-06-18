25e49bd2c1f009befd79788767ced07f82b3dd70
d4n13ls1104 <78829103+d4n13ls1104@users.noreply.github.com>
2021-06-18 01:25:15 -0400

    UPDATE: Refactored slightly
    Refactored slightly and isEmailRegistered & isUsernameRegistered now use promises instead of callbacks.

--- backend/util/sand-validator.ts
+++ backend/util/sand-validator.ts
@@ -1,37 +1,69 @@
 import { escape } from "mysql";
+import { pool } from "./sand-db";
 
-type FunctionBoolCallback = (result: boolean) => void
-
+// -----------------------------------------------------------
 // Purpose: Check if email is valid
+// -----------------------------------------------------------
 export const isEmailValid = (email: string): boolean => {
     return (/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/).test(email.toLowerCase());
 }
 
+// -----------------------------------------------------------
+// Purpose: Check if username is valid
+// -----------------------------------------------------------
 export const isUsernameValid = (username: string): boolean => {
     return (/^[a-zA-Z0-9\_]+$/).test(username);
 }
 
-// Purpose: Check if password meets standards
+// -----------------------------------------------------------
+// Purpose: Check if password meets our standards
+// -----------------------------------------------------------
 export const isPasswordValid = (password: string): boolean => {
     return (/^(?=.*[\d])(?=.*[A-Z])(?=.*[a-z])(?=.*[!@#$%^&*])[\w!@#$%^&*]{8,}$/).test(password);
 }
 
+// -----------------------------------------------------------
 // Purpose: Check if email is already registered
-export const isEmailRegistered = (email: string, connection: any, callback: FunctionBoolCallback) => {
-    connection.query(`SELECT 1 FROM users WHERE email=${escape(email)}`, (err: any, result: any[]) => {
-        if(err) console.error(err);
-        if(result.length > 0) return callback(true);
-    });
+// -----------------------------------------------------------
+export const isEmailRegistered = (email: string): Promise<boolean> => {
+    return new Promise((resolve, reject) => {
+        pool.getConnection((err, connection) => {
+            if(err) {
+                console.error(err);
+                reject("Something went wrong. Please try again later.");
+            }
+            connection.query(`SELECT 1 FROM users WHERE email='${escape(email)}'`, (err, result) => {
+                if(err) {
+                    console.error(err);
+                    reject("Something went wrong. Please try again later.");
+                }
 
-    callback(false);
+                if(result.length > 0) resolve(true);
+            });
+            connection.release();
+        });
+    });
 }
 
-// Purpose: Check if email is already registered
-export const isUsernameRegistered = (username: string, connection: any, callback: FunctionBoolCallback) => {
-    connection.query(`SELECT 1 FROM users WHERE username=${escape(username)}`, (err: any, result: any[]) => {
-        if(err) console.error(err);
-        if(result.length > 0) return callback(true);
-    });
+// -----------------------------------------------------------
+// Purpose: Check if username is already registered
+// -----------------------------------------------------------
+export const isUsernameRegistered = (username: string): Promise<boolean> => {
+    return new Promise((resolve, reject) => {
+        pool.getConnection((err, connection) => {
+            if(err) {
+                console.error(err);
+                reject("Something went wrong. Please try again later");
+            }
+            connection.query(`SELECT 1 FROM users WHERE username='${escape(username)}'`, (err, result) => {
+                if(err) {
+                    console.error(err);
+                    reject("Something went wrong. Please try again later.");
+                }
 
-    callback(false);
+                if(result.length > 0) resolve(true);
+            });
+            connection.release();
+        });
+    });
 }
