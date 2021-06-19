beb3cca393faa3c3cd836594aa595f408960bcb2
d4n13ls1104 <78829103+d4n13ls1104@users.noreply.github.com>
2021-06-19 07:36:07 -0400

    ADD: Send message API

--- backend/routes/api/channels.ts
+++ backend/routes/api/channels.ts
@@ -1,8 +1,8 @@
-import { Router } from "express";
+import { response, Router } from "express";
 import { checkChannelExists, fetchMessagesFromChannelBeforeDate } from "../../util/sand-channel";
 //import { pool } from "../../util/sand-db";
 import { extractPayloadFromToken } from "../../util/sand-jwt";
-import { isUserChannelMember } from "../../util/sand-user";
+import { isUserChannelMember, sendMessageForUser } from "../../util/sand-user";
 import * as validator from "../../util/sand-validator";
 
 const router = Router();
@@ -27,6 +27,33 @@ router.get("/:id/messages", (req, res) => {
     // Fetch messagees
     fetchMessagesFromChannelBeforeDate(parseInt(req.params.id), beforeDate).then(response => {
         return res.json(response);
+    }).catch(reason => { return res.json({ errors: [reason] }); });
+});
+
+//---------------------------------------------------------------
+// Purpose: Send message to channel
+//---------------------------------------------------------------
+router.post("/:id/messages", (req, res) => {
+    if(!req.body.content) return res.sendStatus(400).json({ errors: ["Message cannot be empty."] });
+
+    const content = validator.sanitizeParam(req.body.content);
+
+    if(content.length > 4000) return res.json({ errors: ["Something went wrong. Please try again later."] });
+
+    // Check if channel exists
+    checkChannelExists(parseInt(req.params.id))
+    .catch(reason => { return res.json({ errors: [reason] }); });
+
+    // Get payload from user's auth token
+    const payload = extractPayloadFromToken(req.cookies.auth);
+
+    isUserChannelMember(payload.sub, parseInt(req.params.id))
+    .catch(reason => { return response.json({ errors: [reason] }); });
+
+    // All good, send the message
+    sendMessageForUser(payload.sub, parseInt(req.params.id), content)
+    .then(() => {
+        return res.json({ ok: true });
     }).catch(reason => { return res.json({ errors: [reason] }); });
 });
 
