97416ce14313d05b30f66d07c5498869275f12ec
d4n13ls1104 <78829103+d4n13ls1104@users.noreply.github.com>
2021-06-28 17:14:43 -0400

    REFACTOR: Use styled-components

--- frontend/src/components/Common/Error.tsx
+++ frontend/src/components/Common/Error.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import styled from "styled-components";
 
 interface Props {
     message: string
@@ -6,10 +7,24 @@ interface Props {
 
 const Error: React.FC<Props> = ({ message }) => {
     return (
-        <div className="error">
+        <ErrorWrapper>
             <p>{message}</p>
-        </div>
+        </ErrorWrapper>
     );
 }
+
+const ErrorWrapper = styled.div`
+    background-color: #FECACA;
+    color: #f00;
+    width: 100%;
+    margin-top: 10px;
+    border-radius: 5px;
+    display: flex;
+    justify-content: left;
+    padding-left: 20px;
+    padding-right: 20px;
+    box-sizing: border-box;
+    align-self: flex-end;
+`
 
 export default Error;
