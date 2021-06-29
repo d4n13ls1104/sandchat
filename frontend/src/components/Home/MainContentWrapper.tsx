1af4555277f9ae1bd38b1089abc7155865882553
d4n13ls1104 <78829103+d4n13ls1104@users.noreply.github.com>
2021-06-28 23:51:40 -0400

    Create MainContentWrapper.tsx

---
+++ frontend/src/components/Home/MainContentWrapper.tsx
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+const MainContentWrapper = styled.div`
+    display: flex;
+    flex-grow: 4;
+    box-sizing: border-box;
+    flex-direction: column;
+`
+
+export default MainContentWrapper;
