c944f72f0d082a595f021bedefd9a932988347d5
d4n13ls1104 <78829103+d4n13ls1104@users.noreply.github.com>
2021-06-28 23:54:05 -0400

    Create NavWrapper.tsx

---
+++ frontend/src/components/Home/NavWrapper.tsx
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+const NavWrapper = styled.div`
+    display: flex;
+    height: 100%;
+    flex-grow: 1;
+    min-width: 275px;
+    max-width: 275px;
+    flex-direction: column;
+    justify-content: flex-end;
+    background-color: #1e252f;
+`
+
+export default NavWrapper;
