37043a87d59402079d66c83e258dbcdf48772c06
d4n13ls1104 <78829103+d4n13ls1104@users.noreply.github.com>
2021-06-28 23:54:55 -0400

    Create SettingsButton.tsx

---
+++ frontend/src/components/Home/SettingsButton.tsx
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+const SettingsButton = styled.img`
+    align-self: center;
+    margin-left: auto;
+    margin-right: 20px;
+    padding: 0px;
+`
+
+export default SettingsButton;
