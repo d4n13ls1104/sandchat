4217de55da8cc67adc39a6deb39ed60783ddbd33
d4n13ls1104 <78829103+d4n13ls1104@users.noreply.github.com>
2021-06-28 23:51:04 -0400

    Create BaseWrapper.tsx

---
+++ frontend/src/components/Home/BaseWrapper.tsx
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+const BaseWrapper = styled.div`
+    display: flex;
+    width: 100%;
+    height: 100%;
+    position: fixed;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    left: 0;
+`
+
+export default BaseWrapper
