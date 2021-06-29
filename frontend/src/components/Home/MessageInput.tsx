9ef0c2c1811d1b1afb13c2b96d5dd7798a10ede9
d4n13ls1104 <78829103+d4n13ls1104@users.noreply.github.com>
2021-06-28 23:53:00 -0400

    Create MessageInput.tsx

---
+++ frontend/src/components/Home/MessageInput.tsx
@@ -0,0 +1,23 @@
+import styled from "styled-components";
+
+const MessageInput = styled.input`
+    max-height: 55px;
+    min-height:55px;
+    width: 95%;
+    background-color: #1e242d;
+    border: none;
+    border-radius: 5px;
+    box-sizing: border-box;
+    padding: 8px;
+    color: #dee3ea;
+    font-family:Roboto, sans-serif;
+    font-size: 16px;
+    outline: none;
+    margin-top: auto;
+    flex-grow: 1;
+    margin-left: auto;
+    margin-right:auto;
+    margin-bottom: 6px;
+`
+
+export default MessageInput;
