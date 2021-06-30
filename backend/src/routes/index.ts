8a69702ebeffd655672a1d512965cf4fed817031
Santeeisweird9 <57926741+Santeeisweird9@users.noreply.github.com>
2021-06-29 19:09:19 -0700

    Fix file routing

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -5,7 +5,7 @@ const router = Router();
 
 router.get("*", (req, res) => {
     if((req.baseUrl + req.path) === "/login" && req.cookies.auth) return res.redirect("/home");
-    res.sendFile(resolve(__dirname, "../public/index.html"));
+    res.sendFile(resolve(__dirname, "../../public/index.html"));
 });
 
 export default router;
