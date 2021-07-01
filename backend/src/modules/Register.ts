5c78b6cef843f3b3744fab22e9b1238033293bdd
Andrew Daniels <markassbrownly555@gmail.com>
2021-06-30 21:02:16 -0400

    Add RegisterResolver

---
+++ backend/src/modules/Register.ts
@@ -0,0 +1,26 @@
+import "reflect-metadata";
+import { hash } from "bcryptjs";
+import { User } from "src/entity/User";
+import {
+    Resolver,
+    Mutation,
+    Arg,
+} from "type-graphql";
+
+import { RegisterInput } from "src/modules/user/register/RegisterInput";
+
+@Resolver()
+export class RegisterResolver {
+    @Mutation(() => User, { nullable: true })
+    async register(@Arg("data") { email, username, password }: RegisterInput): Promise<User> {
+        const hashedPassword = await hash(password, 12);
+
+        const user = await User.create({
+            email,
+            username,
+            password: hashedPassword
+        }).save();
+
+        return user;
+    }
+}
