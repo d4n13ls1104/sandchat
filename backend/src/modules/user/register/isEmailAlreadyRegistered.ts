b091acd29fd2761491ff282671bfef5dc1c47eee
Andrew Daniels <markassbrownly555@gmail.com>
2021-07-01 16:52:33 -0400

    Rename IsEmailAlreadyExists validation constraint

---
+++ backend/src/modules/user/register/isEmailAlreadyRegistered.ts
@@ -0,0 +1,30 @@
+import {
+  registerDecorator,
+  ValidationOptions,
+  ValidatorConstraint,
+  ValidatorConstraintInterface,
+} from "class-validator";
+
+import { User } from "../../../entity/User";
+
+@ValidatorConstraint({ async: true })
+export class IsEmailAlreadyRegisteredConstraint implements ValidatorConstraintInterface {
+  async validate(email: string) {
+    return User.findOne({ where: { email } }).then((user) => {
+      if(user) return false;
+      return true;
+    });
+  }
+}
+
+export const IsEmailAlreadyRegistered = (validationOptions?: ValidationOptions) => {
+  return (object: Object, propertyName: string) => {
+    registerDecorator({
+      target: object.constructor,
+      propertyName: propertyName,
+      options: validationOptions,
+      constraints: [],
+      validator: IsEmailAlreadyRegisteredConstraint
+    });
+  }
+}
