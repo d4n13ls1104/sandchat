d32809479ce32ca8b87f0058536b41dfd39c02d3
Andrew Daniels <markassbrownly555@gmail.com>
2021-07-02 03:27:05 -0400

    Add confirmation email to register resolver

--- backend/src/modules/Register.ts
+++ backend/src/modules/Register.ts
@@ -3,6 +3,8 @@ import { hash } from "bcryptjs";
 import { User } from "../entity/User";
 import { Resolver, Mutation, Arg, Query } from "type-graphql";
 import { RegisterInput } from "./user/register/RegisterInput";
+import { sendEmail } from "./utils/sendEmail";
+import { createConfirmationUrl } from "./utils/createConfirmationUrl";
 
 @Resolver()
 export class RegisterResolver {
@@ -22,6 +24,16 @@ export class RegisterResolver {
 			username,
 			password: hashedPassword
 		}).save();
+
+		const verificationUrl = await createConfirmationUrl(user.id);
+
+		await sendEmail({
+			from: '"noreply" <noreply@chat.sandtee.tk>',
+			to: email,
+			subject: "Please confirm your email",
+			text: "Test",
+			html: `<a href="${verificationUrl}">${verificationUrl}</a>`
+		});
 
 		return user;
 	}
