f9647ef783a2aea25b5352606d1c8961a47bac76
Andrew Daniels <markassbrownly555@gmail.com>
2021-07-06 07:13:43 -0400

    Add createForgotPasswordUrl

---
+++ backend/src/modules/utils/createForgotPasswordUrl.ts
@@ -0,0 +1,10 @@
+import { v4 } from "uuid";
+import { redis } from "../../redis";
+import { forgotPasswordPrefix } from "../constants/redisPrefixes";
+
+export const createForgotPasswordUrl = async (userId: number) => {
+    const token = v4();
+    await redis.set(forgotPasswordPrefix + token, userId, "ex", 60 * 60 * 24);
+
+    return `https://chat.sandtee.tk/user/change-password/${token}`;
+}
