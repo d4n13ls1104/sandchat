4aeeb5ccb892b79a54c1884a515608faf1f9447a
Andrew Daniels <markassbrownly555@gmail.com>
2021-07-05 20:36:56 -0400

    Move user related resolvers to user folder

---
+++ backend/src/modules/user/ConfirmEmail.ts
@@ -0,0 +1,20 @@
+import "reflect-metadata";
+import { User } from "../../entity/User";
+import { Resolver, Mutation, Arg } from "type-graphql";
+import { redis } from "../../redis";
+import { confirmUserPrefix } from "../constants/redisPrefixes";
+
+@Resolver()
+export class ConfirmEmailResolver {
+    @Mutation(() => Boolean)
+    async confirmEmail(@Arg("token") token: string): Promise<boolean> {
+        const userId = await redis.get(confirmUserPrefix + token);
+
+        if(!userId) return false; 
+
+        await User.update({ id: parseInt(userId, 10) }, { confirmedEmail: true });
+        await redis.del(confirmUserPrefix + token);
+
+        return true;
+    }
+}
---
+++ backend/src/modules/user/Login.ts
@@ -0,0 +1,30 @@
+import "reflect-metadata";
+import { compare } from "bcryptjs";
+import { User } from "../entity/User";
+import { Resolver, Mutation, Arg, Ctx } from "type-graphql";
+import { LoginInput } from "./user/login/LoginInput";
+import { SandContext } from "../types/SandContext";
+import { SandSession } from "../types/SandSession";
+
+@Resolver()
+export class LoginResolver {
+    @Mutation(() => User, { nullable: true })
+    async login(
+        @Arg("data") { email, password }: LoginInput,
+        @Ctx() ctx: SandContext
+    ): Promise<User> {
+        const user = await User.findOne({ where: { email } });
+
+        if(!user) throw Error("User does not exist.");
+
+        if(!user.confirmedEmail) throw Error("Please confirm your email.");
+
+        const passwordIsValid = await compare(password, user.password);
+
+        if(!passwordIsValid) throw Error("Invalid credentials.");
+
+        (ctx.req.session as SandSession).userId = user.id;
+
+        return user;
+    }
+}
