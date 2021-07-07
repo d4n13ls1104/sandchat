aec3a53d504104f7c57fdbd9c6f057ebe6a12be5
Andrew Daniels <markassbrownly555@gmail.com>
2021-07-07 00:06:56 -0400

    Remove placeholder query

--- backend/src/modules/user/Register.ts
+++ backend/src/modules/user/Register.ts
@@ -1,18 +1,13 @@
 import "reflect-metadata";
 import { hash } from "bcryptjs";
 import { User } from "../../entity/User";
-import { Resolver, Mutation, Arg, Query } from "type-graphql";
+import { Resolver, Mutation, Arg } from "type-graphql";
 import { RegisterInput } from "../user/register/RegisterInput";
 import { sendEmail } from "../utils/sendEmail";
 import { createConfirmationUrl } from "../utils/createConfirmationUrl";
 
 @Resolver()
 export class RegisterResolver {
-	@Query(() => String)
-	async hello() {
-		return "Placeholder query so graphql shuts its fucking mouth";
-	}
-
 	@Mutation(() => User)
 	async register(
 		@Arg("data") { email, username, password }: RegisterInput
