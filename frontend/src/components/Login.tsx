db9ace19110107a01ca4e51b7798a8b3842f80f4
Andrew Daniels <markassbrownly555@gmail.com>
2021-07-07 00:59:21 -0400

    Rewrite Login.tsx to work with the new GraphQL API

--- frontend/src/components/Login.tsx
+++ frontend/src/components/Login.tsx
@@ -1,26 +1,50 @@
-import React, { useEffect, useState } from "react";
+import React, { useState, useEffect, useRef } from "react";
+import { gql, useMutation } from "@apollo/client";
 
 import Error from "components/Common/Error";
 import Link from "components/Common/Link";
+import Success from "components/Common/Success";
 
 import FormWrapper from "components/FormComponents/FormWrapper";
 import FormHeader from "components/FormComponents/FormHeader";
 import FormInput from "components/FormComponents/FormInput";
 import FormButton from "components/FormComponents/FormButton";
-import FormSubText from "components/FormComponents/FormSubText";
 import FormAlertWrapper from "components/FormComponents/FormAlertWrapper";
+import FormSubText from "components/FormComponents/FormSubText";
 
-import axios from "axios";
+interface initialStateInterface {
+    email: string,
+    password: string,
+    success: boolean
+}
 
 const initialState = {
     email: "",
     password: "",
-    success: false,
-    errors: []
+    success: false
 };
 
+const LOGIN_MUTATION = gql`
+mutation LoginUser($email: String!, $password: String!) {
+    login(data: { email: $email, password: $password }) {
+        id
+    }
+}
+`
+
 const Login: React.FC = () => {
-    const [state, setState] = useState<typeof initialState>(initialState);
+    const [state, setState] = useState<initialStateInterface>(initialState);
+    const [error, setError] = useState<string | undefined>();
+
+    const [loginUser, { data, loading }] = useMutation(LOGIN_MUTATION, {
+        onError: ({ graphQLErrors }) => {
+            setState({...state, success: false});
+            setError(graphQLErrors[0].message);
+        }
+    });
+
+    const stateRef = useRef() as React.MutableRefObject<initialStateInterface>;
+    stateRef.current = state;
 
     useEffect(() => {
         window.addEventListener("keydown", handleKeyDown);
@@ -28,37 +52,43 @@ const Login: React.FC = () => {
         return () => {
             window.removeEventListener("keydown", handleKeyDown);
         }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
     }, []);
 
     const handleKeyDown = (e: KeyboardEvent) => {
         if(e.key === "Enter") handleSubmit();
     }
 
-    const handleSubmit = () => {
-        axios.post("/api/user/auth", `email=${state.email}&password=${state.password}`).then(({data}) => {
-            if(data.ok) window.location.href = "/home";
-            if(data.errors) setState({...state, success: false, errors: data.errors});
-        }).catch(err => console.error(err));
+    const handleSubmit = async () => {
+        await loginUser({
+            variables: {
+                email: stateRef.current.email,
+                password: stateRef.current.password
+            }
+        }).then((result) => {
+            if(result.data) setState({...state, success: true});
+        });
     }
 
     return (
         <>
         <FormAlertWrapper>
-            {state.errors.length > 0 ? <Error message={state.errors[0]}/> : null}
+            {error !== undefined && !state.success ? <Error message={error}/> : null}
+            {data && data.login ? <Success message="Logged in!"/> : null}
         </FormAlertWrapper>
 
         <FormWrapper>
             <FormHeader>Login</FormHeader>
             <FormInput onChange={(e) => setState({...state, email: e.target.value})} value={state.email} type="email" placeholder="Email address"/>
-            <FormInput onChange={(e) => setState({...state, password: e.target.value})} value={state.password} type="password" placeholder="Password"/>
-            <FormButton onClick={handleSubmit}>Login</FormButton>
+            <FormInput onChange={(e) => setState({...state, password: e.target.value})} value={state.password} type="password" placeholder="Password"/> 
+            <FormButton onClick={handleSubmit} loading={loading}>Login</FormButton>
 
             <FormSubText>
                 Don't have an account? <Link href="/register">Register</Link>!
             </FormSubText>
         </FormWrapper>
         </>
-    );
+    )
 }
 
 export default Login;
