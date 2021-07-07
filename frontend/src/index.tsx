35ded42438ca38bb32b620bad2889fe787cfd3e6
Andrew Daniels <markassbrownly555@gmail.com>
2021-07-07 00:10:38 -0400

    Add ApolloClient

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,10 +1,15 @@
-import React from 'react';
 import ReactDOM from 'react-dom';
 import App from "App";
+import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client";
+
+const client = new ApolloClient({
+  uri: "http://localhost:4000/graphql",
+  cache: new InMemoryCache()
+});
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <ApolloProvider client={client}>
+    <App/>
+  </ApolloProvider>,
   document.getElementById('root')
 );
