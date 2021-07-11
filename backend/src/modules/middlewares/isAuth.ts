5c85dfd64fd729afcd1b08e2db2fb7aa42376786
Andrew Daniels <markassbrownly555@gmail.com>
2021-07-11 10:07:41 -0400

    Update import paths

--- backend/src/modules/middlewares/isAuth.ts
+++ backend/src/modules/middlewares/isAuth.ts
@@ -1,6 +1,6 @@
 import { MiddlewareFn } from "type-graphql";
-import { SandContext } from "../../types/SandContext";
-import { SandSession } from "../../types/SandSession";
+import { SandContext } from "../../type/SandContext";
+import { SandSession } from "../../type/SandSession";
 
 export const isAuth: MiddlewareFn<SandContext> = async ({ context }, next) => {
     if(!(context.req.session as SandSession).userId) throw Error("Not authenticated");
