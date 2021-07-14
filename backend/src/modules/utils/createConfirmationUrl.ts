45f3ee7249997166027d2cf50547a9a253e0fb87
Andrew Daniels <markassbrownly555@gmail.com>
2021-07-14 12:44:36 -0400

    Change userId to string

--- backend/src/modules/utils/createConfirmationUrl.ts
+++ backend/src/modules/utils/createConfirmationUrl.ts
@@ -2,7 +2,7 @@ import { v4 } from "uuid";
 import { redis } from "../../redis";
 import { confirmUserPrefix } from "../constants/redisPrefixes";
 
-export const createConfirmationUrl = async (userId: number) => {
+export const createConfirmationUrl = async (userId: string) => {
     const token = v4();
     await redis.set(confirmUserPrefix + token, userId, "ex", 60 * 60 * 24);
 
