db223bdc8c2cf791f17488f642d3c48af2945a7b
Andrew Daniels <markassbrownly555@gmail.com>
2021-07-14 12:46:15 -0400

    Change channelId to string

--- frontend/src/gql/Mutations.ts
+++ frontend/src/gql/Mutations.ts
@@ -1,7 +1,7 @@
 import { gql } from "@apollo/client"
 
 export const CREATE_MESSAGE_MUTATION = gql`
-    mutation CreateMessage($channelId: Float!, $content: String!) {
+    mutation CreateMessage($channelId: String!, $content: String!) {
         createMessage(data: {channelId: $channelId, content: $content}) {
             id
         }
