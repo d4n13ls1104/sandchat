094bad20fc6f96cff77746fac1a63aa61ec3905b
Andrew Daniels <markassbrownly555@gmail.com>
2021-07-14 12:45:09 -0400

    Change userId to string

--- backend/src/modules/utils/createForgotPasswordUrl.ts
+++ backend/src/modules/utils/createForgotPasswordUrl.ts
@@ -2,7 +2,7 @@ import { v4 } from "uuid";
 import { redis } from "../../redis";
 import { forgotPasswordPrefix } from "../constants/redisPrefixes";
 
-export const createForgotPasswordUrl = async (userId: number) => {
+export const createForgotPasswordUrl = async (userId: string) => {
     const token = v4();
     await redis.set(forgotPasswordPrefix + token, userId, "ex", 60 * 60 * 24);
 
