8b26e2745feadf43596fab3f83cf3c05cab7b593
Andrew Daniels <markassbrownly555@gmail.com>
2021-07-22 10:17:52 -0400

    Add settings route

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,6 +6,7 @@ import Index from "components/Index";
 import Register from "components/Register";
 import Login from "components/Login";
 import Home from "components/Home";
+import Settings from "components/Settings";
 
 const App: React.FC = () => {
   return (
@@ -14,6 +15,7 @@ const App: React.FC = () => {
       <Route exact path="/register" component={Register}/>
       <Route exact path="/login" component={Login}/>
       <Route exact path="/home" component={Home}/>
+      <Route exact path="/settings" component={Settings}/>
     </Router>
   );
 }
