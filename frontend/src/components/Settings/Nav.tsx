b2a5fa3bce716d7ca0d61a457c47bd4ba9815699
Andrew Daniels <markassbrownly555@gmail.com>
2021-07-22 10:20:58 -0400

    Add Nav component (changing name later

---
+++ frontend/src/components/Settings/Nav.tsx
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const Nav = styled.div`
+    display: flex;
+    flex-direction: column;
+    background-color: #1e252f;
+    width:175px;
+    height: 100%;
+`
