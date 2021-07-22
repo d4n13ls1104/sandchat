5744d1de1c26ed19d447779dadd079f2ec4a94ab
Andrew Daniels <markassbrownly555@gmail.com>
2021-07-22 10:21:17 -0400

    Add other mutations

--- frontend/src/gql/Mutations.ts
+++ frontend/src/gql/Mutations.ts
@@ -7,3 +7,19 @@ export const CREATE_MESSAGE_MUTATION = gql`
         }
     } 
 `
+
+export const LOGIN_MUTATION = gql`
+mutation LoginUser($email: String!, $password: String!) {
+    login(data: { email: $email, password: $password }) {
+        id
+    }
+}
+`
+
+export const REGISTER_MUTATION = gql`
+mutation RegisterUser($email: String!, $username: String!, $password: String!) {
+    register(data: {email: $email, username: $username, password: $password }) {
+        id
+    }
+}
+`
