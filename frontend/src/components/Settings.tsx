732379ac958fcf858c063a87a3cf37de8a4a6fdb
Andrew Daniels <markassbrownly555@gmail.com>
2021-07-22 10:20:27 -0400

    Add Settings component

---
+++ frontend/src/components/Settings.tsx
@@ -0,0 +1,14 @@
+import { Nav } from "components/Settings/Nav";
+import BaseWrapper from "components/Home/BaseWrapper";
+
+const Settings: React.FC = () => {
+    return (
+        <BaseWrapper>
+            <Nav>
+                Test
+            </Nav>
+        </BaseWrapper>
+    );
+}
+
+export default Settings;
