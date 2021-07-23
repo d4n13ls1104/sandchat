6547a737b2ec99873d9e38a35a8db15af74c91f8
Andrew Daniels <markassbrownly555@gmail.com>
2021-07-22 23:10:53 -0400

    Remove unnecessesary validation

--- backend/src/modules/resolvers/user/changeUsername/ChangeUsernameInput.ts
+++ backend/src/modules/resolvers/user/changeUsername/ChangeUsernameInput.ts
@@ -1,4 +1,3 @@
-import { IsNotBlank } from "modules/resolvers/message/createMessage/IsNotBlank";
 import { InputType, Field } from "type-graphql";
 import { Length, Matches } from "class-validator";
 import { IsUsernameAlreadyRegistered } from "../register/isUsernameAlreadyRegistered";
@@ -9,6 +8,5 @@ export class ChangeUsernameInput {
     @Length(1, 16, { message: "Username must be 1-16 characters." })
     @Matches("^[a-zA-Z0-9_.-]*$", undefined, { message: "Username may only contain letters, numbers or: '_', '.', and '-'." })
     @IsUsernameAlreadyRegistered({ message: "Username is already registered." })
-    @IsNotBlank()
     username: string;
 }
